fcbc742cc5918934652bf1b166a2b10b5dde7b95
KarlaAide-Garciacst2 <46895430+KarlaAide-Garciacst2@users.noreply.github.com>
2019-02-18 20:02:15 -0600

    Practica1

---
+++ KarlaGarcia180219StoryBoard/KarlaGarcia180219StoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KarlaGarcia180219StoryBoard
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 19-02-18.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
